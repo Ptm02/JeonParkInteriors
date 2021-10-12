73c566d8a732801e68401d2b90f829dca698334f
Pratima Rajput <pratimabulbul02@gmail.com>
2021-10-12 21:04:49 +0530

    changes 9p.m.

--- main.js
+++
@@ -1,9 +0,0 @@
-let processScroll = () => {
-  let docElem = document.documentElement,
-      docBody = document.Body,
-      scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
-      scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) -window.innerHeight,
-      scrollPercent = scrollTop/scrollBottom * 100 + '%';
-
-      console.log(scrollTop + '||' + scrollBottom + '||' +scrollPercent);
-}
